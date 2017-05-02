e7aef833493c29c006dd2c2c306bf3ae7659c1a2
candyalladi <kalyan.alladi@gmail.com>
2017-05-02 21:05:28 +0530

    Added Dropdown menu to Login User name

---
+++ SocialMedia/TweetnHash/Migrations/201705021455090_AddedNameProperty.cs
@@ -0,0 +1,18 @@
+namespace TweetnHash.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedNameProperty : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "Name", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "Name");
+        }
+    }
+}
